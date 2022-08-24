61d6fd4c69fc2db824a16d564245e1d84dc09486
GertSallaerts <1267900+GertSallaerts@users.noreply.github.com>
2022-08-24 15:47:08 +0200

    temp

--- apps/bare-expo/scripts/start-emulator.sh
+++ apps/bare-expo/scripts/start-emulator.sh
@@ -11,29 +11,29 @@ echo " ☛  Bootstrapping Expo in ${CURRENT_ENV} mode"
 
 "$DIR/setup-android-project.sh"
 
-if ! $ANDROID_SDK_ROOT/tools/android list avd | grep -q bare-expo; then
-    echo " ⚠️  No emulator for bare Expo found, creating one..."
-    $DIR/create-emulator.sh 22
-fi
+# if ! $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/avdmanager list avd | grep -q bare-expo; then
+#     echo " ⚠️  No emulator for bare Expo found, creating one..."
+#     $DIR/create-emulator.sh 22
+# fi
 
-if $ANDROID_SDK_ROOT/platform-tools/adb devices -l | grep -q emulator; then
-    echo " ✅ Emulator is already running"
-else
-    echo " ⚠️  Starting emulator..."
-    echo "no" | $ANDROID_SDK_ROOT/emulator/emulator "-avd" "bare-expo" "-skin" "480x800" "-no-audio" "-no-boot-anim" "-port" "5554" "-no-snapshot" "-partition-size" "1024" &
+# if $ANDROID_SDK_ROOT/platform-tools/adb devices -l | grep -q emulator; then
+#     echo " ✅ Emulator is already running"
+# else
+#     echo " ⚠️  Starting emulator..."
+#     echo "no" | $ANDROID_SDK_ROOT/emulator/emulator "-avd" "bare-expo" "-skin" "480x800" "-no-audio" "-no-boot-anim" "-port" "5554" "-no-snapshot" "-partition-size" "1024" &
 
-    $DIR/wait-for-emulator.sh
-    sleep 30
+#     $DIR/wait-for-emulator.sh
+#     sleep 30
 
-    $ANDROID_SDK_ROOT/platform-tools/adb wait-for-device
-    $ANDROID_SDK_ROOT/platform-tools/adb shell settings put global window_animation_scale 0
-    $ANDROID_SDK_ROOT/platform-tools/adb shell settings put global transition_animation_scale 0
-    $ANDROID_SDK_ROOT/platform-tools/adb shell settings put global animator_duration_scale 0
-    $ANDROID_SDK_ROOT/platform-tools/adb shell am broadcast -a android.intent.action.BOOT_COMPLETED &
+#     $ANDROID_SDK_ROOT/platform-tools/adb wait-for-device
+#     $ANDROID_SDK_ROOT/platform-tools/adb shell settings put global window_animation_scale 0
+#     $ANDROID_SDK_ROOT/platform-tools/adb shell settings put global transition_animation_scale 0
+#     $ANDROID_SDK_ROOT/platform-tools/adb shell settings put global animator_duration_scale 0
+#     $ANDROID_SDK_ROOT/platform-tools/adb shell am broadcast -a android.intent.action.BOOT_COMPLETED &
 
-    sleep 5
-    echo " ✅ Emulator is running"
-fi
+#     sleep 5
+#     echo " ✅ Emulator is running"
+# fi
 
 $DIR/start-metro.sh $port
 
